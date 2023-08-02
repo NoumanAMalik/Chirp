773a27c3dca0952b9cd4713f6bf3d6ab3911d04c
Nouman <noumancodes@gmail.com>
2023-08-02 00:31:23 -0400

    Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'Chirp',
-    description: 'Twitter Clone by Nouman and Danny',
-}
+    title: "Chirp",
+    description: "Twitter Clone by Nouman and Danny",
+};
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html data-theme="dark">
             <body className={inter.className}>{children}</body>
         </html>
-    )
+    );
 }
